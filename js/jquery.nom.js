92debe596025ec4ac7c08675d8745fef0bbcd1e6
Xing Zhou <xing@xingzhou.me>
2012-10-17 23:17:10 -0400

    Add git ignore file

--- js/jquery.nom.js
+++ js/jquery.nom.js
@@ -1,5 +1,12 @@
+// # jQuery.nom
+//
+// jQuery.nom (nom) captures the DOM elements as text or blob along with a screenshot.
+// This information can be sent to the server to enable better user feedback.
+//
+// Goal:
 (function( $ ) {
   $.fn.nom = function() {
-  
+
+    return this; // Return the jQuery object passed into the function, to maintain extensibility
   };
 })( jquery );
