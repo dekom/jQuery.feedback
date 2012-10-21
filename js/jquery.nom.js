971ff7f3e37d07fe9b7957705fb72424a7fd64f2
Xing Zhou <xing@xingzhou.me>
2012-10-21 17:47:16 -0400

    Add basic method call functions.

--- js/jquery.nom.js
+++ js/jquery.nom.js
@@ -1,12 +1,30 @@
-// # jQuery.nom
-//
-// jQuery.nom (nom) captures the DOM elements as text or blob along with a screenshot.
-// This information can be sent to the server to enable better user feedback.
-//
-// Goal:
-(function( $ ) {
-  $.fn.nom = function() {
+// **jQuery.nom (nom)** captures the DOM elements as text or blob along with a
+// screenshot.  This information can be sent to the server to enable better
+// user feedback.
 
-    return this; // Return the jQuery object passed into the function, to maintain extensibility
+(function( $ ) {
+  'use strict';
+
+  var methods = {
+    init: function( options ) {
+
+      var settings = $.extend( {
+        classes: ['nom']
+      }, options );
+    }
   };
-})( jquery );
+
+  // Handles method calls
+  $.fn.nom = function( method  ) {
+    // Return the jQuery object passed into the function, to maintain
+    // extensibility
+    if (methods[method]) {
+      return methods[method].apply( this,
+        Array.prototype.slice.call(arguments, 1) );
+    } else if ( typeof method === 'object' || !method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' + method + ' does not exist on jQuery.nom ' );
+    }
+  }; // end $.fn.nom
+})( jQuery ); // end (function($))
--- static_server.js
+++ static_server.js
@@ -1,13 +1,19 @@
-var http = require("http")
-,   url = require("url")
-,   path = require("path")
-,   fs = require("fs")
-,   port = process.argv[2] || 8888;
+// **Static Server** is just a plain nodejs server capable of serving
+// html, javascript, css, and plain text files with the correct mime
+// definition.
+//
+// Default URL: http://localhost:8888/
+
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8888;
 
 http.createServer(function(request, response) {
 
-  var uri = url.parse(request.url).pathname
-  ,   filename = path.join(process.cwd(), uri);
+  var uri = url.parse(request.url).pathname,
+      filename = path.join(process.cwd(), uri);
 
   fs.exists(filename, function(exists) {
     if(!exists) {
@@ -15,9 +21,9 @@ http.createServer(function(request, response) {
       response.write("404 Not Found\n");
       response.end();
       return;
-    }          
+    }
 
-    if (fs.statSync(filename).isDirectory()) { filename += '/index.html'}
+    if (fs.statSync(filename).isDirectory()) { filename += '/index.html'; }
 
     fs.readFile(filename, "binary", function(err, file) {
       if(err) {
@@ -25,9 +31,29 @@ http.createServer(function(request, response) {
         response.write(err + "\n");
         response.end();
         return;
-      }          
+      }
 
-      response.writeHead(200);
+      // Content-type to be returned
+      // This is determined using a simple switch statement.  This should
+      // really be abstracted to another module that can determine the
+      // content-type based on the basename(filename).
+      var content_type;
+
+      switch ( path.extname(filename) ) {
+        case ('.js'):
+          content_type = 'application/javascript';
+          break;
+        case ('.css'):
+          content_type = 'text/css';
+          break;
+        case ('.html'):
+          content_type = 'text/html';
+          break;
+        default:
+          content_type = 'text/plain';
+      }
+
+      response.writeHead(200, {"Content-Type": content_type});
       response.write(file, "binary");
       response.end();
     });
