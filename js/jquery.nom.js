570e394ba8ec9dc0a0691b886236186869c628f6
Xing Zhou <xing@xingzhou.me>
2012-10-22 12:58:03 -0400

    Setup API interface

--- js/jquery.nom.js
+++ js/jquery.nom.js
@@ -1,16 +1,59 @@
 // **jQuery.nom (nom)** captures the DOM elements as text or blob along with a
 // screenshot.  This information can be sent to the server to enable better
 // user feedback.
+//
+// TODO Create external API to guide planning and coding
+//
+//  External API:
+//  init: to setup parameters for nomming
+//  activate: to enable DOM element selection for consumption
+//  select: to select the element for consumption
+//  output: outputs the serialized version of the selected elements
 
 (function( $ ) {
   'use strict';
 
   var methods = {
-    init: function( options ) {
-
+    //  Init(options, cb_fn):
+    //    @params an options object to configure jQuery.nom.
+    //    @return $(this)
+    //
+    //    Configures the plugin.
+    //
+    //    The valid options are:
+    //      classes:  an array of class attributes that can be selected for
+    //      feedback
+    init: function( options, cb_fn ) {
       var settings = $.extend( {
         classes: ['nom']
       }, options );
+
+      return $(this);
+    },
+
+    //  Active(cb_fn):
+    //    @return $(this)
+    //
+    //    Readies the plugin to accept elements for reporting
+    active: function( cb_fn ) {
+      return $(this);
+    },
+
+    //  Select(element, cb_fn):
+    //    @params the element that's to be selected
+    //    @return $(this)
+    //
+    //    Adds the element to the list of elements that needs to be reported
+    select: function( element, cb_fn ) {
+      return $(this);
+    },
+
+    //  Output(cb_fn):
+    //    @return $(this)
+    //
+    //    Outputs a JSON object of all the elements collected.
+    output: function( cb_fn ) {
+      return $(this);
     }
   };
 
