6084567420df6fc2e6337b6fdd784f9f6d0c906b
Xing Zhou <xing@xingzhou.me>
2012-10-22 23:09:41 -0400

    Comment on the public API

--- js/jquery.nom.js
+++ js/jquery.nom.js
@@ -13,54 +13,58 @@
 (function( $ ) {
   'use strict';
 
+  //# Public Methods
+  //
+  // All methods return the jQuery object passed into the function, 
+  // to maintain extensibility
   var methods = {
-    //  Init(options, cb_fn):
-    //    @params an options object to configure jQuery.nom.
-    //    @return $(this)
+
+    //##  Init(options, cb_fn):
     //
-    //    Configures the plugin.
+    //      @params an options object to configure jQuery.nom.
     //
-    //    The valid options are:
-    //      classes:  an array of class attributes that can be selected for
-    //      feedback
+    //      Configures the plugin.
+    //
+    //      The valid options are:
+    //        classes:  an array of class attributes that can be selected
+    //        for feedback
     init: function( options, cb_fn ) {
       var settings = $.extend( {
         classes: ['nom']
       }, options );
 
       return $(this);
-    },
+    }, // end init()
 
-    //  Active(cb_fn):
-    //    @return $(this)
+    //##  Active(cb_fn):
     //
-    //    Readies the plugin to accept elements for reporting
+    //      @return $(this)
+    //
+    //      Readies the plugin to accept elements for reporting
     active: function( cb_fn ) {
       return $(this);
-    },
+    }, // end active()
 
-    //  Select(element, cb_fn):
-    //    @params the element that's to be selected
-    //    @return $(this)
+    //##  Select(element, cb_fn):
     //
-    //    Adds the element to the list of elements that needs to be reported
+    //      @params the element that's to be selected
+    //
+    //      Adds the element to the list of elements that needs to be
+    //      reported
     select: function( element, cb_fn ) {
       return $(this);
-    },
+    }, // end select()
 
-    //  Output(cb_fn):
-    //    @return $(this)
+    //##  Output(cb_fn):
     //
-    //    Outputs a JSON object of all the elements collected.
+    //      Outputs a JSON object of all the elements collected.
     output: function( cb_fn ) {
       return $(this);
-    }
+    } // end output()
   };
 
-  // Handles method calls
+  //# jQuery Methods Handling
   $.fn.nom = function( method  ) {
-    // Return the jQuery object passed into the function, to maintain
-    // extensibility
     if (methods[method]) {
       return methods[method].apply( this,
         Array.prototype.slice.call(arguments, 1) );
@@ -69,5 +73,5 @@
     } else {
       $.error( 'Method ' + method + ' does not exist on jQuery.nom ' );
     }
-  }; // end $.fn.nom
+  }; // end $.fn.nom()
 })( jQuery ); // end (function($))
