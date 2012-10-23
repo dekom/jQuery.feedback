d79549e465d017be62135f6ed40d9fa2975048b7
Xing Zhou <xing@xingzhou.me>
2012-10-23 19:39:26 -0400

    Remove unnecessary node module

--- spec/jquery.nom.spec.js
+++ spec/jquery.nom.spec.js
@@ -5,6 +5,14 @@
 
 describe("jQuery.nom", function() {
   it("should exist as a jQuery function", function() {
-    expect(jQuery.nom).toEqual(jasmine.any(Function));
+    expect(jQuery.fn.nom).toEqual(jasmine.any(Function));
+  });
+});
+
+describe("External API", function() {
+  describe("init", function() {
+    it("should exist as a jQuery function", function() {
+      expect(jQuery.fn.nom("init")).toEqual(jasmine.any(Function));
+    });
   });
 });
